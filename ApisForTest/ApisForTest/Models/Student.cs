0b10698438d03e623a08174062aab01f66b9ec62
suraj <surajwakure007@gmail.com>
2024-10-25 00:32:01 +0530

    Changes made to add the student in the application

---
+++ ApisForTest/ApisForTest/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace ApisForTest.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int STD { get; set;}
+      
+    }
+}
